339af08d1180e3c2ec8603529546f038c8c90dd8
Yuri Alves <yuri.o.alves2@outlook.com>
2022-07-16 00:45:01 -0300

    API Test

---
+++ backend/src/main/java/com/yurialves/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.yurialves.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yurialves.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
